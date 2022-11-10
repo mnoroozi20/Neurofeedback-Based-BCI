61e8482d4cbb1bef741f9c6219818a9bc985a400
Ben Garneau <benjamin.garneau31@gmail.com>
2022-11-10 07:52:39 -0500

    updates and deletions

--- main_gui.py
+++ main_gui.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QPushButton
+
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        #self.setGeometry(50,50,1000,500)
+        # self.setGeometry(50,50,1000,500)
         self.setWindowTitle("Window")
         self.setStyleSheet('background: #505050')
         self.stage = 3
@@ -16,21 +18,21 @@ class Window(QtWidgets.QWidget):
 
         label1 = QtWidgets.QLabel('Patient:')
         label1.setFixedWidth(50)
-        label1.setStyleSheet('font-size: 15px')
+        label1.setStyleSheet('font-size: 15px; ')
 
         self.line = QtWidgets.QLineEdit(self)
         self.line.setFixedWidth(200)
         self.line.setStyleSheet('background: #EEEEEE; margin-right: 20px')
-        
+
         start_btn = QtWidgets.QPushButton('Start', self)
         start_btn.setFixedWidth(75)
         start_btn.setStyleSheet('background: #228C22')
-        start_btn.clicked.connect(self.func) 
+        start_btn.clicked.connect(self.func)
 
         stop_btn = QtWidgets.QPushButton('Stop', self)
         stop_btn.setFixedWidth(75)
         stop_btn.setStyleSheet('background: #B80F0A')
-        stop_btn.clicked.connect(self.func) 
+        stop_btn.clicked.connect(self.func)
 
         label2 = QtWidgets.QLabel('Phase:')
         label2.setFixedWidth(50)
@@ -47,24 +49,60 @@ class Window(QtWidgets.QWidget):
         label3.setFixedWidth(50)
         label3.setStyleSheet('font-size: 15px')
 
-        image = QtGui.QPixmap("C:/Users/tnlab/Desktop/Neurofeedback-Project/Asssets/BlockLoad" + str(self.stage) +"_5.png").scaled(175,175, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        self.button = QPushButton(self)
+        self.button.setFixedWidth(90)
+        self.button.setText('Sub Window')
+        self.button.setStyleSheet('font-size:15px')
+
+        image = QtGui.QPixmap("Assets/BlockLoad" + str(self.stage) + "_5.png").scaled(175, 175, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         block = QtWidgets.QLabel()
         block.setPixmap(image)
         block.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         block.setFixedWidth(175)
 
-        grid.addWidget(label1,0,0)
-        grid.addWidget(self.line,0,1)
-        grid.addWidget(start_btn,0,2)
-        grid.addWidget(stop_btn,0,3)
-        grid.addWidget(label2,1,0)
-        grid.addWidget(combobox,1,1)
-        grid.addWidget(label3,2,0)
-        grid.addWidget(block,2,1)
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(self.line, 0, 1)
+        grid.addWidget(start_btn, 0, 2)
+        grid.addWidget(stop_btn, 0, 3)
+        grid.addWidget(label2, 1, 0)
+        grid.addWidget(combobox, 1, 1)
+        grid.addWidget(label3, 2, 0)
+        grid.addWidget(block, 2, 1)
+        grid.addWidget(self.button, 0, 4)
         self.show()
+
+        # Sub Window
+        self.sub_window = SubWindow()
+        self.button.clicked.connect(self.sub_window.show)
 
     def func(self):
         print(self.line.text())
+
+
+class SubWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(SubWindow, self).__init__()
+        self.resize(400, 300)
+        self.setWindowTitle("SubWindow")
+        self.setStyleSheet('background: #505050')
+        self.stage = 3
+        self.main()
+
+    def main(self):
+        grid = QtWidgets.QGridLayout()
+        self.setLayout(grid)
+
+        start_btn = QtWidgets.QPushButton('Start', self)
+        start_btn.setFixedWidth(75)
+        start_btn.setStyleSheet('background: #228C22')
+
+        stop_btn = QtWidgets.QPushButton('Stop', self)
+        stop_btn.setFixedWidth(75)
+        stop_btn.setStyleSheet('background: #B80F0A')
+
+        grid.addWidget(start_btn, 0, 1)
+        grid.addWidget(stop_btn, 0, 2)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
