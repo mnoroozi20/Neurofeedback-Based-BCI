b229a72d0788087e7de3bd3ae2d5c0bfab49a457
Ben Garneau <benjamin.garneau31@gmail.com>
2022-11-10 12:52:08 -0500

    update to GUI

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 
 
@@ -8,7 +9,7 @@ class Window(QtWidgets.QWidget):
     def __init__(self):
         super(Window, self).__init__()
         # self.setGeometry(50,50,1000,500)
-        self.setWindowTitle("Window")
+        self.setWindowTitle("Main Window")
         self.setStyleSheet('background: #505050')
         self.stage = 3
         self.home()
@@ -88,95 +89,91 @@ class Window(QtWidgets.QWidget):
 class SubWindow(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
-        layout = QtWidgets.QGridLayout(self)
+        self.layout = QtWidgets.QGridLayout(self)
         self.resize(500, 500)
         self.setWindowTitle("Image Window")
         self.setStyleSheet('background: #505050')
 
         self.label = QtWidgets.QLabel()
-        layout.addWidget(self.label, 0, 0, 1, 2)
+        self.layout.addWidget(self.label, 0, 0, 1, 2)
         self.label.setMinimumSize(200, 200)
         # the label alignment property is always maintained even when the contents
         # change, so there is no need to set it each time
         self.label.setAlignment(QtCore.Qt.AlignCenter)
 
         self.loadImageButton = QtWidgets.QPushButton('Load image')
-        layout.addWidget(self.loadImageButton, 1, 0)
+        self.loadImageButton.setStyleSheet("background: #FFFFFF")
+        self.layout.addWidget(self.loadImageButton, 1, 0)
 
         self.nextImageButton = QtWidgets.QPushButton('Next image')
-        layout.addWidget(self.nextImageButton)
+        self.nextImageButton.setStyleSheet("background: #FFFFFF")
+        self.layout.addWidget(self.nextImageButton)
 
-        self.loadImageButton.clicked.connect(self.loadImage)
-        self.nextImageButton.clicked.connect(self.nextImage)
+        self.loadImageButton.clicked.connect(self.load_image)
+        self.nextImageButton.clicked.connect(self.next_image)
 
         self.dirIterator = None
         self.fileList = []
 
-    def loadImage(self):
-        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Select Image', '', 'Image Files (*.png *.jpg *.jpeg)')
-        if filename:
-            pixmap = QtGui.QPixmap(filename).scaled(self.label.size(), QtCore.Qt.KeepAspectRatio)
-            if pixmap.isNull():
-                return
-            self.label.setPixmap(pixmap)
-            dirpath = os.path.dirname(filename)
-            self.fileList = []
-            for f in os.listdir(dirpath):
-                fpath = os.path.join(dirpath, f)
-                if os.path.isfile(fpath) and f.endswith(('.png', '.jpg', '.jpeg')):
-                    self.fileList.append(fpath)
-            self.fileList.sort()
-            self.dirIterator = iter(self.fileList)
-            while True:
-                # cycle through the iterator until the current file is found
-                if next(self.dirIterator) == filename:
-                    break
+        self.count = 2
 
-    def nextImage(self):
-        # ensure that the file list has not been cleared due to missing files
-        if self.fileList:
-            try:
-                filename = next(self.dirIterator)
-                pixmap = QtGui.QPixmap(filename).scaled(self.label.size(), QtCore.Qt.KeepAspectRatio)
-                if pixmap.isNull():
-                    # the file is not a valid image, remove it from the list
-                    # and try to load the next one
-                    self.fileList.remove(filename)
-                    self.nextImage()
-                else:
-                    self.label.setPixmap(pixmap)
-            except:
-                # the iterator has finished, restart it
-                self.dirIterator = iter(self.fileList)
-                self.nextImage()
-        else:
-            # no file list found, load an image
-            self.loadImage()
+    # def load_image(self):
+    #     filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Select Image', '', 'Image Files (*.png *.jpg *.jpeg)')
+    #     if filename:
+    #         pixmap = QtGui.QPixmap(filename).scaled(self.label.size(), QtCore.Qt.KeepAspectRatio)
+    #         if pixmap.isNull():
+    #             return
+    #         self.label.setPixmap(pixmap)
+    #         dirpath = os.path.dirname(filename)
+    #         self.fileList = []
+    #         for f in os.listdir(dirpath):
+    #             fpath = os.path.join(dirpath, f)
+    #             if os.path.isfile(fpath) and f.endswith(('.png', '.jpg', '.jpeg')):
+    #                 self.fileList.append(fpath)
+    #         self.fileList.sort()
+    #         self.dirIterator = iter(self.fileList)
+    #         while True:
+    #             # cycle through the iterator until the current file is found
+    #             if next(self.dirIterator) == filename:
+    #                 break
+    #
+    # def next_image(self):
+    #     # ensure that the file list has not been cleared due to missing files
+    #     if self.fileList:
+    #         try:
+    #             filename = next(self.dirIterator)
+    #             pixmap = QtGui.QPixmap(filename).scaled(self.label.size(), QtCore.Qt.KeepAspectRatio)
+    #             if pixmap.isNull():
+    #                 # the file is not a valid image, remove it from the list
+    #                 # and try to load the next one
+    #                 self.fileList.remove(filename)
+    #                 self.nextImage()
+    #             else:
+    #                 self.label.setPixmap(pixmap)
+    #         except:
+    #             # the iterator has finished, restart it
+    #             self.dirIterator = iter(self.fileList)
+    #             self.nextImage()
+    #     else:
+    #         # no file list found, load an image
+    #         self.loadImage()
 
+    def load_image(self):
 
-    # def __init__(self):
-    #     super(SubWindow, self).__init__()
-    #     self.resize(400, 300)
-    #     self.setWindowTitle("SubWindow")
-    #     self.setStyleSheet('background: #505050')
-    #     self.stage = 3
-    #     self.main()
-    #
-    # def main(self):
-    #     grid = QtWidgets.QGridLayout()
-    #     self.setLayout(grid)
-    #
-    #     start_btn = QtWidgets.QPushButton('Start', self)
-    #     start_btn.setFixedWidth(75)
-    #     start_btn.setStyleSheet('background: #228C22')
-    #
-    #     close_btn = QtWidgets.QPushButton('Close', self)
-    #     close_btn.clicked.connect(lambda: self.close())
-    #     close_btn.setFixedWidth(75)
-    #     close_btn.setStyleSheet('background: #B80F0A')
-    #
-    #     grid.addWidget(start_btn, 0, 1)
-    #     grid.addWidget(close_btn, 0, 2)
+        pixmap = QPixmap('Images/1.jpg')
+        self.label.setPixmap(pixmap)
+        self.resize(pixmap.width(), pixmap.height())
+        self.show()
+
+        # Connect button to image updating
+
+    def next_image(self):
+        # pic = QtGui.QLabel(self)
+        # pic.setGeometry(100, 10, 800, 800)
+        image_bank = ["Images/2.jpg", "Images/3.jpg", "Images/4.jpg", "Images/5.jpg", ]
+
+        self.label.setPixmap(QtGui.QPixmap(f"Images/{self.count}.jpg"))
+        self.count += 1
 
 
 if __name__ == '__main__':
