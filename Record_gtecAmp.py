1986372b71a2acda8ec20395a322d23d0a41c4e4
Maryam Norouzi <123695744+maryamnorouzi@users.noreply.github.com>
2023-02-01 13:35:36 -0500

    Update Record_gtecAmp.py

--- Record_gtecAmp.py
+++ Record_gtecAmp.py
@@ -1,18 +1,29 @@
 
-import csv 
 import pygds as g
+import pygds
+import numpy as np
+import pandas as pd
+
 d = g.GDS()
+
 minf_s = sorted(d.GetSupportedSamplingRates()[0].items())[0]
 d.SamplingRate, d.NumberOfScans = minf_s
+
 for ch in d.Channels:
     ch.Acquire = True
 d.SetConfiguration()
-scope=g.Scope(1/d.SamplingRate)
-data = d.GetData(d.SamplingRate,scope)
+scope = pygds.Scope(1/d.SamplingRate, title="Channels: %s", ylabel = u"U[μV]")
 
-with open('egg_data.csv', 'w') as file:
-    writer = csv.writer(file)
-    for row in data:
-        writer.writerow(data)
-d.Close()
-del(d)
+Matrixdata=[]
+event_time=int(input('Enter the time of event:'))
+i=0
+while i<event_time:
+    aa = d.GetData(d.SamplingRate)
+    Matrixdata.extend(aa)   
+    i+=1
+my_array = np.array(Matrixdata)
+
+# print(my_array)
+# print(type(my_array))
+# print(my_array.shape)
+pd.DataFrame(my_array).to_excel('my_matrix.xlsx')
